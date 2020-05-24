042b45a0c8f7fc368a04ad95bf913f36b1ffaece
Danny Veldhoen <danny.veldhoen@dashweb.nl>
2020-05-24 17:57:40 +0200

    Added linting

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 var axios = require('axios');
 
 class easyinvoice {
-
-    static createInvoice(options, cb = () => {
+    static createInvoice (options, cb = () => {
     }) {
         return new Promise((resolve, reject) => {
-            var url = "https://api.factuursimpel.nl/v1/invoices";
+            var url = 'https://api.factuursimpel.nl/v1/invoices';
 
             const data = {
                 data: options
             };
             axios.post(url, data)
                 .then(function (response) {
-                    var result = response['data']['data'];
+                    var result = response.data.data;
                     resolve(result);
                     cb(result);
                 })
