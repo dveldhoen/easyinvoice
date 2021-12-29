85b13741ec7612dd7210b9ec9b44a1e9c418cc0a
Danny Veldhoen <danny.veldhoen@factuursimpel.nl>
2021-12-29 12:06:22 +0100

    Added libraries back so it will work through CDN

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,6 @@ function generateConfig(name) {
         mode: environment,
         entry: './index.js',
         target: 'node',
-        externals: {
-            axios: 'axios',
-            isBase64: 'is-base64',
-            FileSaver: 'file-saver'
-        },
         output: {
             filename: [name] + '.js',
             sourceMapFilename: [name] + '.map',
